19ad8193fe5ce42dba42728aca030b3f85bec5a1
ybayraktutan <74928622+ybayraktutan@users.noreply.github.com>
2021-04-24 22:55:46 +0100

    Level 1 completed
    Level 1 completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import babyNamesData from './babyNamesData.json'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Baby Name Picker</h2>
       </header>
+      <div>
+        {babyNamesData.sort((babyName1, babyName2) => (
+           babyName1.name.localeCompare(babyName2.name)
+         ))
+         .map((babyName, index) => (
+           babyName.sex == "f" ? 
+           <span key = {index} className = "female">{babyName.name} </span>
+           :<span key = {index} className = "male">{babyName.name} </span>
+          ))}
+      </div>
     </div>
   );
 }
 
 export default App;
+
